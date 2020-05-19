1e647767fbb099db86d2200fecdb070907143cf5
Michael White <mwhite9@apple.com>
2020-05-19 00:14:03 -0700

    switch to using pandas; update gitignore

--- analyze_data.py
+++ analyze_data.py
@@ -1,21 +1,27 @@
 import numpy as np
-import datetime as dt
+import pandas as pd
 
+# load population data for a county
 def load_county_pop(county, state):
     import csv
     with open('county_pop.csv', newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             if row[0] == county + " County":
-                return int(row[2])
+                return float(row[2])
 
+# load case and death data for a county
+def load_county_cd(csv_data, county, state):
+    r = csv_data[np.logical_and(csv_data['county'] == county, csv_data['state'] == state)]
+    return np.array(r[['cases', 'deaths']])
+
+# plot cases over time and new cases versus total cases
 def plot_county_list(county_list):
-    csv_data = np.loadtxt('covid-19-data/us-counties.csv', delimiter=',', skiprows=1,
-                          dtype={'names': ('date', 'county', 'state', 'fips', 'cases', 'deaths'),
-                                 'formats': ('S10', 'S32', 'S32', 'S5', 'i4', 'i4')})
+    csv_data = pd.read_csv('covid-19-data/us-counties.csv')
     for (county, state) in county_list:
-        print((county, state))
-        print(load_county_pop(county, state))
+        ccd = load_county_cd(csv_data, county, state)
+        pop = load_county_pop(county, state)
+        print(ccd)
 
 if __name__ == "__main__":
-    plot_county_list([("Santa Clara", "California"), ("Los Angeles", "California"), ("Broward", "Florida")])
+    plot_county_list([('Santa Clara', 'California'), ('Los Angeles', 'California'), ('Broward', 'Florida')])

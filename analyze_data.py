7ce6a04152d3cd37d9ea92f1a42df18e5e1fafbc
Michael White <mwhite9@apple.com>
2020-07-05 11:26:29 -0700

    added sum of sigmoids fit

--- analyze_data.py
+++ analyze_data.py
@@ -54,49 +54,57 @@ def smooth_iir_1(y):
     return (z0 + z1) * 0.5
 
 
-# generalized logistic function (sigmoid)
-def sigmoid(a, t):
-    return a[0] / ((a[1] + np.exp(-a[2] * (t - a[3]))) ** a[4])
+# sum of sigmoids (generalized logistic functions)
+def sigmoid_sum(a, t):
+    assert(len(a) % 5 == 0)
+    m = len(a) // 5
+    ss = 0
+    for i in range(m):
+        ss += a[5*i] / ((a[5*i+1] + np.exp(-a[5*i+2] * (t - a[5*i+3]))) ** a[5*i+4])
+    return ss
 
 
-# compute the Jacobian of the residual in a generalized logistic fit
-def sigmoid_jac(a, t, w):
+# compute the Jacobian of the residual in a sum of sigmoids fit
+def sigmoid_sum_jac(a, t, w):
+    assert(len(a) % 5 == 0)
+    m = len(a) // 5
     n = t.shape[0]
-    jac = np.zeros((n, 5))
-    e = np.exp(-a[2] * (t - a[3]))
-    d = (a[1] + e)
-    dp = w * d ** (-a[4]) / np.sqrt(n)
-    dpd = w * a[0] * (-a[4]) * (d ** (-a[4] - 1)) / np.sqrt(n)
-    jac[:, 0] = dp
-    jac[:, 1] = dpd
-    jac[:, 2] = dpd * (-(t - a[3])) * e
-    jac[:, 3] = dpd * a[2] * e
-    jac[:, 4] = a[0] * -dp * np.log(d)
+    jac = np.zeros((n, len(a)))
+    for i in range(m):
+        e = np.exp(-a[5*i+2] * (t - a[5*i+3]))
+        d = (a[5*i+1] + e)
+        dp = w * d ** (-a[5*i+4]) / np.sqrt(n)
+        dpd = w * a[5*i+0] * (-a[5*i+4]) * (d ** (-a[5*i+4] - 1)) / np.sqrt(n)
+        jac[:, 5*i+0] = dp
+        jac[:, 5*i+1] = dpd
+        jac[:, 5*i+2] = dpd * (-(t - a[5*i+3])) * e
+        jac[:, 5*i+3] = dpd * a[5*i+2] * e
+        jac[:, 5*i+4] = a[5*i+0] * -dp * np.log(d)
     return jac
 
 
-def sigmoid_jac_test():
+def sigmoid_sum_jac_test():
     n = 100
     t = np.linspace(0., 1., n)
     w = np.ones(n)
-    a = np.array([1.5, 1., 6., 0.8, 1.])
+    a = np.array([1.5, 1, 6, 0.8, 1, 2, 0.5, 3, 1, 1.1])
     y0 = np.random.rand(n)
     # direct computation of jacobian of residual
-    jac0 = sigmoid_jac(a, t, w)
+    jac0 = sigmoid_sum_jac(a, t, w)
     # finite difference approximation
-    jac1 = np.zeros((n, 5))
+    jac1 = np.zeros((n, 10))
     eps = 1e-6
-    res0 = w * (sigmoid(a, t) - y0) / np.sqrt(n)
-    for i in range(5):
+    res0 = w * (sigmoid_sum(a, t) - y0) / np.sqrt(n)
+    for i in range(10):
         a1 = a.copy()
         a1[i] += eps
-        res1 = w * (sigmoid(a1, t) - y0) / np.sqrt(n)
+        res1 = w * (sigmoid_sum(a1, t) - y0) / np.sqrt(n)
         jac1[:, i] = (res1 - res0) / eps
     print((jac0 - jac1).max(0))
 
 
-# fit a sigmoid using the Gauss-Newton method to optimize weighted least squares
-def fit_sigmoid_wls_gn(x_init, t, w, y0_raw, iter_max, debug=False):
+# fit a sigmoid sum using the Gauss-Newton method to optimize weighted least squares
+def fit_sigmoid_sum_wls_gn(x_init, t, w, y0_raw, iter_max, debug=False):
     # initialize return value
     x_opt = x_init.copy()
 
@@ -106,18 +114,18 @@ def fit_sigmoid_wls_gn(x_init, t, w, y0_raw, iter_max, debug=False):
     y0 = y0_raw / y0_max
 
     # Levenberg-Marquardt regularization parameters
-    tol = 1e-3
-    eps_min = 1e-5
-    eps_max = 1e+3
+    tol = 1e-2
+    eps_min = 1e-6
+    eps_max = 1e+1
     eps_inc = 3
-    eps_dec = 0.66
+    eps_dec = 0.5
 
     # set up initial values
-    eps = 0.01
+    eps = 0.1
     eps_it = 0
     x1 = x_init.copy()
-    res = w * (sigmoid(x1, t) - y0) / np.sqrt(n)
-    jac = sigmoid_jac(x1, t, w)
+    res = w * (sigmoid_sum(x1, t) - y0) / np.sqrt(n)
+    jac = sigmoid_sum_jac(x1, t, w)
     err1 = np.linalg.norm(res)
     err_opt = err1
 
@@ -128,11 +136,12 @@ def fit_sigmoid_wls_gn(x_init, t, w, y0_raw, iter_max, debug=False):
         x0 = x1.copy()
         x1 -= np.linalg.solve(jac.T @ jac + eps * np.eye(n), jac.T @ res) * 0.5
         x1 = np.maximum(x1, 1e-2)
+        x1 = np.minimum(x1, 1e+2)
 
         # update weighted residual and its Jacobian
         err0 = err1
-        res = w * (sigmoid(x1, t) - y0) / np.sqrt(n)
-        jac = sigmoid_jac(x1, t, w)
+        res = w * (sigmoid_sum(x1, t) - y0) / np.sqrt(n)
+        jac = sigmoid_sum_jac(x1, t, w)
         err1 = np.linalg.norm(res)
 
         # lock in parameters if the error goes down
@@ -160,23 +169,29 @@ def fit_sigmoid_wls_gn(x_init, t, w, y0_raw, iter_max, debug=False):
 
         # possibly output debug information
         if debug:
-            print(f'Iter: {it:2d},    Eps: {eps:.5f},    Err: {err1:.5f},    Best Err: {err_opt:.5f}')
+            print(f'Iter: {it:2d},    Eps: {eps:.6f},    Err: {err1:.6f},    Best Err: {err_opt:.6f}')
     if debug:
         print()
-    x_opt[0] *= y0_max
+    x_opt[::5] *= y0_max
     return x_opt
 
 
 # fit sigmoid function (generalized logistic) using iteratively reweighted least squares
-def fit_sigmoid(y0, n_ext):
+def fit_sigmoid_sum(y0, n_ext, n_sigmoids):
     n = len(y0)
     t0 = np.linspace(0., 1., n)
     w0 = np.ones(n)
-    a0 = np.array([1.5, 1, 5, 0.75, 1])
-    a1 = fit_sigmoid_wls_gn(a0, t0, w0, y0, 200)
+    w0[-10:] = 2
+    alpha = 30 * np.polyfit(np.arange(7), y0[-7:], 1)[0] / np.max(y0)
+    assert(n_sigmoids <= 2)
+    a0 = np.array([(1 - alpha) * 1.5, 1, 15, 0.3, 1, alpha * 1.5, 1, 15, 1, 1])
+    if n_sigmoids == 1:
+        a0 = a0[:5]
+    a1 = fit_sigmoid_sum_wls_gn(a0, t0, w0, y0, 200)
+    print(a1)
     t1 = np.concatenate((t0, 1. + (t0[1] - t0[0]) * (np.arange(n_ext) + 1.)))
-    y1 = sigmoid(a1, t1)
-    y1_max = sigmoid(a1, 100.)
+    y1 = sigmoid_sum(a1, t1)
+    y1_max = sigmoid_sum(a1, 100.)
     return y1, y1_max
 
 
@@ -191,11 +206,11 @@ def est_growth_rate(y):
 
 
 # plot cases and deaths over time
-def plot_cd_county(ax, cd, county, state, date, pop):
+def plot_cd_county(ax, cd, county, state, date, pop, n_sigmoids):
     # number of days to plot and extrapolate
     n_days = cd.shape[0]
-    n_days_ext = 10
-    n_days_plot = 40
+    n_days_ext = 20
+    n_days_plot = 120
 
     # normalize by population (cases/deaths per 100,000)
     c0 = cd[:, 0] / pop * 1e5
@@ -204,8 +219,8 @@ def plot_cd_county(ax, cd, county, state, date, pop):
     # fit sigmoid to cases and deaths
     t0 = np.flip(np.arange(n_days) + 1.)
     t1 = np.flip(np.arange(-n_days_ext, n_days) + 1.)
-    c1, c1_max = fit_sigmoid(smooth_iir_1(c0), n_days_ext)
-    d1, d1_max = fit_sigmoid(smooth_iir_1(d0), n_days_ext)
+    c1, c1_max = fit_sigmoid_sum(smooth_iir_1(c0), n_days_ext, n_sigmoids)
+    d1, d1_max = fit_sigmoid_sum(smooth_iir_1(d0), n_days_ext, n_sigmoids)
 
     # title
     ax.set_title('{} County, {} - {}'.format(county, state, date), {'fontsize': 10})
@@ -223,28 +238,28 @@ def plot_cd_county(ax, cd, county, state, date, pop):
     ax.set_ylabel('Cases (per 100k)')
     ax.set_xlabel('Days ago')
     ax.set_xlim(n_days_plot, -n_days_ext)
-    ax.set_ylim(0, 1.2 * np.max(c0))
+    ax.set_ylim(0, 1.25 * np.max(c0))
 
     # plot deaths
     ax2.plot(t1, d1, color='xkcd:baby pink')
     ax2.plot(t0, d0, color='tab:red')
     ax2.set_ylabel('Deaths (per 100k)', color='tab:red')
     ax2.tick_params(axis='y', labelcolor='tab:red')
-    ax2.set_ylim(0, max(2 * np.max(d0), 20))
+    ax2.set_ylim(0, 2.5 * np.max(d0))
 
 
 # plot new cases versus total cases over time
-def plot_nc_county(ax, cd, county, state, date, pop):
+def plot_nc_county(ax, cd, county, state, date, pop, n_sigmoids):
     # number of days to plot and extrapolate
     n_days = cd.shape[0]
-    n_days_ext = 10
+    n_days_ext = 20
     c0 = cd[:, 0]
     c0s = smooth_iir_1(c0)
 
     # fit sigmoid to cases
     t0 = np.flip(np.arange(n_days) + 1.)
     t1 = np.flip(np.arange(-n_days_ext, n_days) + 1.)
-    c1, c1_max = fit_sigmoid(c0s, n_days_ext)
+    c1, c1_max = fit_sigmoid_sum(c0s, n_days_ext, n_sigmoids)
 
     # get new cases per day
     dc0 = np.diff(c0s)
@@ -260,9 +275,9 @@ def plot_nc_county(ax, cd, county, state, date, pop):
     c1 = c1[idx:]
 
     # plot cases
-    ax.loglog(c1, dc1, color='tab:gray')
-    ax.loglog(c0, dc0, color='k', marker='.')
+    ax.loglog(c0, dc0, color='tab:gray', marker='.')
     ax.loglog(c0[-1], dc0[-1], color='r', marker='o')
+    ax.loglog(c1, dc1, color='k')
     ax.set_ylabel('Cases Per Day (smoothed)')
     ax.set_xlabel('Total Cases')
 
@@ -278,13 +293,13 @@ def plot_county_list(county_list):
         county, state = county_list[i]
         cd = load_cd_county(csv_data, county, state)
         pop = load_pop_county(county, state)
-        plot_cd_county(axs[0, i], cd, county, state, date, pop)
-        plot_nc_county(axs[1, i], cd, county, state, date, pop)
+        plot_cd_county(axs[0, i], cd, county, state, date, pop, 2)
+        plot_nc_county(axs[1, i], cd, county, state, date, pop, 2)
 
 
 # table of new deaths per day for the top 10 states
 def plot_nd_states():
-    n_days = 30
+    n_days = 50
     states, cd_states, date_str = load_cd_states()
     n_states = len(states)
     # compute growth rates and their corresponding scores
@@ -293,28 +308,32 @@ def plot_nd_states():
     for i in range(n_states):
         pop = load_pop_state(states[i])
         gr.append(est_growth_rate(smooth_iir_1(cd_states[i][-n_days-7:, 0])) / pop * 1e5)
-        gr_score[i] = np.mean(gr[i][-10:]) + np.max(gr[i]) + 10 * (pop > 2e5)
+        gr_score[i] = (np.mean(gr[i][-14:]) + (np.max(gr[i]) - np.min(gr[i])) / 10) * (pop > 2e5)
     # sort based on recent growth rate
     idx = np.argsort(gr_score)
     gr = [gr[i] for i in idx]
     st = [states[i] for i in idx]
     # plot worst states
-    n_top = 10
+    line_styles = ['r-', 'r--', 'r-.', 'r:', 'k-', 'k--', 'k-.', 'k:', 'b-', 'b--', 'b-.', 'b:']
+    n_top = 8
     fig = plt.figure(figsize=(8, 5))
     x = np.arange(n_days, 0, -1)
+    curve_idx = 0
     for i in range(n_top - 1, -1, -1):
         n = min(len(gr[n_states - n_top + i]), n_days)
-        plt.plot(x[-n:], gr[n_states - n_top + i][-n:], label=st[n_states - n_top + i], lw=2.0)
+        plt.plot(x[-n:], gr[n_states - n_top + i][-n:], line_styles[curve_idx], label=st[n_states - n_top + i], lw=2.0)
+        curve_idx += 1
     # add specific states
-    plot_states = ['California', 'Florida']
+    plot_states = ['California']
     for i in range(len(plot_states)):
         j = st.index(plot_states[i])
         if j >= n_top:
             n = min(len(gr[j]), n_days)
-            plt.plot(x[-n:], gr[j][-n:], label=st[j], lw=2.0)
+            plt.plot(x[-n:], gr[j][-n:], line_styles[curve_idx], label=st[j], lw=2.0)
+            curve_idx += 1
     plt.xlabel('Days Ago')
     plt.ylabel('New Cases per 100k (Weekly Average)')
-    plt.xlim(n_days, 1)
+    plt.xlim(n_days, 4)
     plt.legend(loc='upper left', fontsize='small')
     plt.title(date_str)
 
